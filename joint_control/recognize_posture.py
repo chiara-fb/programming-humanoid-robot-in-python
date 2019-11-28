b8db50445a06b22b094c6a34eaaed4f9ed4ba718
chiara-fb <56759395+chiara-fb@users.noreply.github.com>
2019-11-28 17:15:15 +0100

    Add files via upload

--- joint_control/recognize_posture.py
+++ joint_control/recognize_posture.py
@@ -8,11 +8,15 @@
     Let the robot execute different keyframes, and recognize these postures.
 
 '''
+#versione finale
 
-
+import pickle, io, numpy as np
+from os import *
 from angle_interpolation import AngleInterpolationAgent
-from keyframes import hello
+from keyframes import *
 
+ROBOT_POSE_DATA_DIR = 'robot_pose_data'
+ROBOT_POSE_PKL = 'robot_pose.pkl'
 
 class PostureRecognitionAgent(AngleInterpolationAgent):
     def __init__(self, simspark_ip='localhost',
@@ -22,7 +26,7 @@ class PostureRecognitionAgent(AngleInterpolationAgent):
                  sync_mode=True):
         super(PostureRecognitionAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.posture = 'unknown'
-        self.posture_classifier = None  # LOAD YOUR CLASSIFIER
+        self.posture_classifier = pickle.load(io.open(ROBOT_POSE_PKL))
 
     def think(self, perception):
         self.posture = self.recognize_posture(perception)
@@ -30,11 +34,24 @@ class PostureRecognitionAgent(AngleInterpolationAgent):
 
     def recognize_posture(self, perception):
         posture = 'unknown'
-        # YOUR CODE HERE
+        features = ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 
+                    'RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch']
+
+        postures = ['Frog', 'Knee', 'Stand', 'Crouch', 'Right', 'Sit', 
+                    'Belly', 'Back', 'StandInit', 'HeadBack', 'Left']
+        
+        
+        pred_data  = [self.perception.joint[i] for i in features] +self.perception.imu
+
+        p_ix = self.posture_classifier.predict(np.array(pred_data).reshape(1,-1))[0]
+        
+        posture = postures[p_ix]
+        #print(posture)
+
 
         return posture
 
 if __name__ == '__main__':
     agent = PostureRecognitionAgent()
-    agent.keyframes = hello()  # CHANGE DIFFERENT KEYFRAMES
+    agent.keyframes = leftBellyToStand()  # CHANGE DIFFERENT KEYFRAMES
     agent.run()
